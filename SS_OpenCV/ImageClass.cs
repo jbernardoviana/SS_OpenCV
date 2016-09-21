fc6a06d3a10ff12bea5ca4f98f6bb649c1d4ee30
JB <JB@JB-PC>
2016-09-21 18:05:59 +0100

    Add project files.

---
+++ SS_OpenCV/ImageClass.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Emgu.CV.Structure;
+using Emgu.CV;
+
+namespace SS_OpenCV
+{
+    class ImageClass
+    {
+
+        /// <summary>
+        /// Image Negative using EmguCV library
+        /// Slower method
+        /// </summary>
+        /// <param name="img">Image</param>
+        internal static void Negative(Image<Bgr, byte> img)
+        {
+            Bgr aux;
+            for (int y = 0; y < img.Height; y++)
+            {
+                for (int x = 0; x < img.Width; x++)
+                {
+                    // emguCV access: slower
+                    aux = img[y, x];
+                    img[y, x] = new Bgr(255 - aux.Blue, 255 - aux.Green, 255 - aux.Red);
+                }
+            }
+        }
+        
+        /// Negative using memory
+        /// 
+        internal static void NegativeMemory(Image<Bgr, byte> img)
+        {
+            unsafe
+            {
+                // direct access to the image memory(sequencial)
+                // direcion top left -> bottom right
+
+                MIplImage m = img.MIplImage;
+                byte* dataPtr = (byte*)m.imageData.ToPointer(); // Pointer to the image
+                byte blue, green, red, gray;
+
+                int width = img.Width;
+                int height = img.Height;
+                int nChan = m.nChannels; // number of channels - 3
+                int padding = m.widthStep - m.nChannels * m.width; // alinhament bytes (padding)
+                int x, y;
+
+                if (nChan == 3) // image in RGB
+                {
+                    for (y = 0; y < height; y++)
+                    {
+                        for (x = 0; x < width; x++)
+                        {
+                            //obtém as 3 componentes
+                            blue = dataPtr[0];
+                            green = dataPtr[1];
+                            red = dataPtr[2];
+
+                            // convert to negative
+                            blue = (byte)(255 - (int)blue);
+                            red = (byte)(255 - (int)red);
+                            green = (byte)(255 - (int)green);
+                    
+                            // store in the image
+                            dataPtr[0] = blue;
+                            dataPtr[1] = green;
+                            dataPtr[2] = red;
+
+                            // advance the pointer to the next pixel
+                            dataPtr += nChan;
+                        }
+
+                        //at the end of the line advance the pointer by the aligment bytes (padding)
+                        dataPtr += padding;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Convert to gray
+        /// Direct access to memory
+        /// </summary>
+        /// <param name="img">image</param>
+        internal static void ConvertToGray(Image<Bgr, byte> img)
+        {
+            unsafe
+            {
+                // direct access to the image memory(sequencial)
+                // direcion top left -> bottom right
+
+                MIplImage m = img.MIplImage;
+                byte* dataPtr = (byte*)m.imageData.ToPointer(); // Pointer to the image
+                byte blue, green, red, gray;
+
+                int width = img.Width;
+                int height = img.Height;
+                int nChan = m.nChannels; // number of channels - 3
+                int padding = m.widthStep - m.nChannels * m.width; // alinhament bytes (padding)
+                int x, y;
+                        
+                if (nChan == 3) // image in RGB
+                {
+                    for (y = 0; y < height; y++)
+                    {
+                        for (x = 0; x < width; x++)
+                        {
+                            //obtém as 3 componentes
+                            blue = dataPtr[0];
+                            green = dataPtr[1];
+                            red = dataPtr[2];
+
+                            // convert to gray
+                            gray = (byte)(((int)blue + green + red) / 3);
+
+                            // store in the image
+                            dataPtr[0] = gray;
+                            dataPtr[1] = gray;
+                            dataPtr[2] = gray;
+
+                            // advance the pointer to the next pixel
+                            dataPtr += nChan;
+                        }
+
+                        //at the end of the line advance the pointer by the aligment bytes (padding)
+                        dataPtr += padding;
+                    }
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ SS_OpenCV/MainForm.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+
+namespace SS_OpenCV
+{
+    public partial class MainForm : Form
+    {
+        Image<Bgr, Byte> img = null; // working image
+        Image<Bgr, Byte> imgUndo = null; // undo backup image - UNDO
+        string title_bak = "";
+
+        public MainForm()
+        {
+            InitializeComponent();
+            title_bak = Text;
+        }
+
+        /// <summary>
+        /// Opens a new image
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                img = new Image<Bgr, byte>(openFileDialog1.FileName);
+                Text = title_bak + " [" +
+                        openFileDialog1.FileName.Substring(openFileDialog1.FileName.LastIndexOf("\\") + 1) +
+                        "]";
+                imgUndo = img.Copy();
+                ImageViewer.Image = img.Bitmap;
+                ImageViewer.Refresh();
+            }
+        }
+
+        /// <summary>
+        /// Saves an image with a new name
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                ImageViewer.Image.Save(saveFileDialog1.FileName);
+            }
+        }
+
+        /// <summary>
+        /// Closes the application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// restore last undo copy of the working image
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (imgUndo == null) // verify if the image is already opened
+                return; 
+            Cursor = Cursors.WaitCursor;
+            img = imgUndo.Copy();
+
+            ImageViewer.Image = img.Bitmap;
+            ImageViewer.Refresh(); // refresh image on the screen
+
+            Cursor = Cursors.Default; // normal cursor 
+        }
+
+        /// <summary>
+        /// Chaneg visualization mode
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void autoZoomToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // zoom
+            if (autoZoomToolStripMenuItem.Checked)
+            {
+                ImageViewer.SizeMode = PictureBoxSizeMode.Zoom;
+                ImageViewer.Dock = DockStyle.Fill;
+            }
+            else // with scroll bars
+            {
+                ImageViewer.Dock = DockStyle.None;
+                ImageViewer.SizeMode = PictureBoxSizeMode.AutoSize;
+            }
+        }
+
+        /// <summary>
+        /// Show authors form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void autoresToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AuthorsForm form = new AuthorsForm();
+            form.ShowDialog();
+        }
+
+
+        /// <summary>
+        /// Convert the working image to grayscale
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void grayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (img == null) // verify if the image is already opened
+                return;
+            Cursor = Cursors.WaitCursor; // clock cursor 
+
+            //copy Undo Image
+            imgUndo = img.Copy();
+
+            ImageClass.ConvertToGray(img);
+
+            ImageViewer.Image = img.Bitmap;
+            ImageViewer.Refresh(); // refresh image on the screen
+
+            Cursor = Cursors.Default; // normal cursor 
+        }
+
+        /// <summary>
+        /// Calculate the image negative
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void negativeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (img == null) // verify if the image is already opened
+                return;
+            Cursor = Cursors.WaitCursor; // clock cursor 
+
+            //copy Undo Image
+            imgUndo = img.Copy();
+
+            ImageClass.Negative(img);
+
+            ImageViewer.Image = img.Bitmap;
+            ImageViewer.Refresh(); // refresh image on the screen
+
+            Cursor = Cursors.Default; // normal cursor 
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Calculate image negative using memory (faster process)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void negativeMemoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (img == null) // verify if the image is already opened
+                return;
+            Cursor = Cursors.WaitCursor; // clock cursor 
+
+            //copy Undo Image
+            imgUndo = img.Copy();
+
+            ImageClass.NegativeMemory(img);
+
+            ImageViewer.Image = img.Bitmap;
+            ImageViewer.Refresh(); // refresh image on the screen
+
+            Cursor = Cursors.Default; // normal cursor 
+        }
+    }
+}
